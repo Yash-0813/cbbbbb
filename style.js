ca4bf8cf7192e17c37237e7804818b99ffef5002
Yash-0813 <90716102+Yash-0813@users.noreply.github.com>
2023-03-03 16:12:42 +0530

    Create style.js

---
+++ style.js
@@ -0,0 +1,24 @@
+.box h1{
+    font-size: 5rem;
+    text-align: center;
+}
+/* @media screen and (max-width:720px){
+    /* h1{
+        font-size: 3rem;
+    } */
+    
+
+@media  screen and(max-width:420px) {
+    /* h1{
+        font-size: 1.8rem;
+    } */
+    .box{
+        background-color: orange;
+    }
+}  
+@media screen and(min-width:432px) and (max-width:720px){
+    
+    .box{
+        background-color: chocolate;
+    }
+}
